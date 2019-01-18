dab62829bdc76b2e2f8361dd924e042bd05fa412
Vince Zipparro <vince.zipparro@coyote.com>
2019-01-18 16:13:09 -0600

    completed training assignment

---
+++ Training_Jan15th/GenerationFinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Jan15th
+{
+    class GenerationFinder
+    {
+
+        public string determination(string year)
+        {
+            var formatter = " ";
+            if (year.Length <= 3 || Convert.ToInt32(year) < 1901 || year == " ")
+            {
+                Console.Write("Invalid year given: " + year);
+            }
+            else if (Convert.ToInt32(year) >= 1901 && Convert.ToInt32(year) <= 1926)
+            {
+                Console.WriteLine("GI Generation: " + year);
+            }
+            else if (Convert.ToInt32(year) >= 1927 && Convert.ToInt32(year) <= 1945)
+            {
+                Console.WriteLine("Mature/Silents: " + year);
+
+            }
+            else if (Convert.ToInt32(year) >= 1946 && Convert.ToInt32(year) <= 1964)
+            {
+                Console.WriteLine("Baby Boomers: " + year);
+
+            }
+            else if (Convert.ToInt32(year) >= 1965 && Convert.ToInt32(year) <= 1980)
+            {
+                Console.WriteLine("Generation X: " + year);
+
+            }
+            else if (Convert.ToInt32(year) >= 1981 && Convert.ToInt32(year) <= 2000)
+            {
+                Console.WriteLine("Generation Y: " + year);
+
+            }
+            else
+            {
+                Console.WriteLine("Generation Z: " + year);
+            }
+
+            return formatter;
+        }
+    }
+}
--- Training_Jan15th/Program.cs
+++ Training_Jan15th/Program.cs
@@ -10,7 +10,24 @@ namespace Training_Jan15th
     {
         static void Main(string[] args)
         {
-    
+            GenerationFinder g = new GenerationFinder();
+
+            do
+            {
+                Console.WriteLine("Enter years of when people were born:");
+                string nums = Console.ReadLine();
+                string[] input = nums.Split(' ');
+
+                foreach (string year in input)
+                {
+                   Console.WriteLine(g.determination(year));
+                }
+
+                Console.ReadKey();
+
+            } while (true);
+
         }
     }
+
 }
